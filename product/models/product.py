cceac61a69fb57b8020e1abd402269ebe429dbbf
Doug <doug.edgates@gmail.com>
2024-06-11 11:25:05 -0300

    creation of models

--- product/models/product.py
+++ product/models/product.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from product.models.category import Category
+from product.models import Category
 
 
 class Product(models.Model):
@@ -8,4 +8,4 @@ class Product(models.Model):
     description = models.TextField(max_length=500, blank=True, null=True)
     price = models.PositiveIntegerField(null=True)
     active = models.BooleanField(default=True)
-    categories = models.ManyToManyField(Category, blank=True)
+    category = models.ManyToManyField(Category, blank=True)
