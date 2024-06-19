b685cfce55eb62a425c8a684e7b5c7628c50fed4
Doug <doug.edgates@gmail.com>
2024-06-19 10:17:13 -0300

    add: tests

--- product/factories.py
+++ product/factories.py
@@ -1,16 +1,12 @@
-
-
-
 import factory
 
 from product.models import Product
 from product.models import Category
 
-
 class CategoryFactory(factory.django.DjangoModelFactory):
     title = factory.Faker('pystr')
     slug = factory.Faker('pystr')
-    description =factory.Faker('pystr')
+    description = factory.Faker('pystr')
     active = factory.Iterator([True, False])
 
     class Meta:
@@ -18,7 +14,7 @@ class CategoryFactory(factory.django.DjangoModelFactory):
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
-    price = factory.Faker('pystr')
+    price = factory.Faker('pyint')
     category = factory.LazyAttribute(CategoryFactory)
     title = factory.Faker('pystr')
 
